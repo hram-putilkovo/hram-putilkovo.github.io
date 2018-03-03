d4672d8c0f13cbd74a35beb25feb319de20d16ab
hram-putilkovo <hram.put@gmail.com>
2018-03-04 02:05:28 +0300

    Update date1.js

--- date1.js
+++ date1.js
@@ -1,3 +1,24 @@
 <--
-d = new Date(); document.write(d);
+d = new Date();
+Year = d.getFullYear();
+Month = d.getMonth();
+Day = d.getDate();
+
+switch (Month)
+{
+  case 0: fMonth="января"; break;
+  case 1: fMonth="февраля"; break;
+  case 2: fMonth="марта"; break;
+  case 3: fMonth="апреля"; break;
+  case 4: fMonth="мае"; break;
+  case 5: fMonth="июня"; break;
+  case 6: fMonth="июля"; break;
+  case 7: fMonth="августа"; break;
+  case 8: fMonth="сентября"; break;
+  case 9: fMonth="октября"; break;
+  case 10: fMonth="ноября"; break;
+  case 11: fMonth="декабря"; break;
+}
+
+document.write("Сегодня "+Day+" "+fMonth+" "+Year+" года");
 -->
